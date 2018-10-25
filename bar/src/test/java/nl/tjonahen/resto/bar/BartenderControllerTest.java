8f7811d141d31fef407655a76160e6d76160df39
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2018-10-25 14:27:06 -0700

    Upgrade springboot 2.0.6

---
+++ bar/src/test/java/nl/tjonahen/resto/bar/BartenderControllerTest.java
@@ -0,0 +1,36 @@
+package nl.tjonahen.resto.bar;
+
+import java.util.ArrayList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ *
+ * @author Philippe Tjon - A - Hen philippe@tjonahen.nl
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class BartenderControllerTest {
+
+    @Mock
+    private BartenderService bartenderServiceMock;
+    
+    @InjectMocks
+    private BartenderController bartenderController;
+            
+
+    @Test
+    public void testGetAllDrinks() {
+        Mockito.when(bartenderServiceMock.getAllDrinks()).thenReturn(new ArrayList<>());
+        Assert.assertNotNull(bartenderController.getAllDrinks());
+    }
+
+    @Test
+    public void testGetDrink() {
+    }
+    
+}
---
+++ bar/src/test/java/nl/tjonahen/resto/bar/BartenderServiceTest.java
@@ -0,0 +1,38 @@
+package nl.tjonahen.resto.bar;
+
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Philippe Tjon - A - Hen philippe@tjonahen.nl
+ */
+public class BartenderServiceTest {
+    
+
+    @Test
+    public void testGetAllDrinksExpect5Drinks() {
+   
+        final BartenderService sut = new BartenderService(null);
+        final List<Drink> drinks = sut.getAllDrinks();
+        
+        Assert.assertEquals(5, drinks.size());
+        
+    }
+
+    @Test
+    public void testServeDrinkExpectRestCall() {
+        
+        RestTemplate restTemplate = Mockito.mock(RestTemplate.class);
+        
+        final BartenderService sut = new BartenderService(restTemplate);
+        Mockito.when(restTemplate.postForLocation("null/api/order/271266/serve/drinks", Void.class)).thenReturn(null);
+        
+        sut.serveDrink(271266L);
+        
+    }
+    
+}
